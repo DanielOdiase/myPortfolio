d807f15340f67e88ba511ccacdf6eda3f7ad683a
Daniel Odiase <82052211+DanielOdiase@users.noreply.github.com>
2022-01-12 19:44:39 -0500

    make some changes to styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,8 @@ import AboutMe from "../../image/AboutMe.JPG"
 export default function About() {
     return (
         
-            <div className="a">
+            <div className="a" id="AboutMe">
+                
              <div className="a-left">
                  <div className="a-card bg"></div>
                  <div className="a-card">
@@ -13,19 +14,17 @@ export default function About() {
                  </div>
 
              </div>
-
-
              <div className="a-right">
              <h1 className="a-title">
                  About Me
              </h1>
              <p className="a-sub">
              So About Me ... Im a well rounded Engineer,with a Major In Mechanical Engineering and Minor in Computer Science . 
-             I  pride myself as a problem solver that uses available resoures to create solutions for existing problems.
-             I love seeing my designs come to life both in the engineering world and in the tech world . 
-             Ive been blessed with the opportunity of working as a project engineer, in which i was able to bring a lot of ideas /designs to life through the entire product life cycle.
-             I've been involved in the design and development of various succesfull, consstruction projects, ecommerce websites and  web-apps.
-             In my free time i love to create quality connections with friends , 
+             I  pride myself as a problem solver who uses available resoures to create solutions for existing problems.
+             I love seeing my designs come to life in the tech world . 
+             Ive been blessed with the opportunity of working as a project manager, in which I was able to bring many ideas and designs to life through the entire product life cycle.
+             I've been involved in the design and development of various succesfull, construction projects, ecommerce websites and  web-apps.
+             In my free time I love to create quality connections with friends , 
              learn new languages, workout, play basketball and learn more about myself and life.
              </p>
              </div>
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -10,6 +10,9 @@ export default function Menu({ menuOpen, setMenuOpen }) {
             <a href="#intro">Home</a>
           </li>
           <li onClick={()=>setMenuOpen(false)}>
+            <a href="#AboutMe">AboutMe</a>
+          </li>
+          <li onClick={()=>setMenuOpen(false)}>
             <a href="#portfolio">Portfolio</a>
           </li>
         
