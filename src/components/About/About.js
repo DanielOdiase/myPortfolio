7aa0a8d02c35c9696be0e1607e5b8f234c585456
Daniel Odiase <82052211+DanielOdiase@users.noreply.github.com>
2022-02-10 21:00:34 -0500

    Design change
    Change the layout and design of portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,31 +4,80 @@ import AboutMe from "../../image/AboutMe.JPG"
 
 export default function About() {
     return (
-        
+        <div className='a-container'> 
+            <h1 className="a-title">
+                     My Skills
+                     </h1>
             <div className="a" id="AboutMe">
-                
              <div className="a-left">
                  <div className="a-card bg"></div>
                  <div className="a-card">
-                     <img src={AboutMe} alt="netflixcloneimage" className="a-img" />
+                     <img src={AboutMe} alt="profileimage" className="a-img" />
                  </div>
 
              </div>
              <div className="a-right">
-             <h1 className="a-title">
-                 About Me
-             </h1>
-             <p className="a-sub">
-             So About Me ... Im a well rounded Engineer,with a Major In Mechanical Engineering and Minor in Computer Science . 
+
+            <div className='a-tagContainer'> 
+                <ul className='a-tag'> 
+                    <li className='tags'>
+                        JavaScript
+                    </li>
+                    <li className='tags'>
+                        TypeScript
+                    </li>
+                    <li className='tags'>
+                        Python
+                    </li>
+                    <li className='tags'>
+                        React
+                    </li>
+                    <li className='tags'>
+                        Node Js
+                    </li>
+                    <li className='tags'>
+                        HTML
+                    </li>
+                    <li className='tags'>
+                        CSS
+                    </li>
+                    <li className='tags'>
+                        Gatsby
+                    </li>
+                    <li className='tags'>
+                        Next Js
+                    </li>
+                    <li className='tags'>
+                        MongoDB
+                    </li>
+                    <li className='tags'>
+                        Express
+                    </li>
+                    <li className='tags'>
+                        GraphQL
+                    </li>
+                    <li className='tags'>
+                        Firebase
+                    </li>
+                    <li className='tags'>
+                        Shopify
+                    </li>
+                    <li className='tags'>
+                        PostgreSQL
+                    </li>
+                </ul>
+            </div>
+            <div className='a-sub'> <p>So About Me ... Im a well rounded Engineer,with a Major In Mechanical Engineering and Minor in Computer Science . 
              I  pride myself as a problem solver who uses available resoures to create solutions for existing problems.
              I love seeing my designs come to life in the tech world . 
              Ive been blessed with the opportunity of working as a project manager, in which I was able to bring many ideas and designs to life through the entire product life cycle.
              I've been involved in the design and development of various succesfull, construction projects, ecommerce websites and  web-apps.
              In my free time I love to create quality connections with friends , 
              learn new languages, workout, play basketball and learn more about myself and life.
-             </p>
+             </p> </div>
              </div>
                 
+            </div>
             </div>
         
     )
--- src/components/portfoliof/Portfolio.js
+++ src/components/portfoliof/Portfolio.js
@@ -64,33 +64,121 @@ export default function Portfolio() {
     //  }, [selected])
     return (
         <div className ="portfolio" id="portfolio">
-            <h1 style={{backgroundColor:"inherit"}}>Portfolio </h1>
+            <h1 style={{backgroundColor:"inherit"}}>Projects </h1>
            {/*<ul className="port">
             {list.map((item)=> 
               <PortfolioList title={item.title} active={selected===item.id} setSelected={setSelected} id={item.id}/>)} 
             </ul> */}
-            <div className="container">
-            <div className='first'>
+            
+            <div className='container'>
+              <div className='p-left'>
+                <h3 className='p-text'> FULL STACK DEVELOPER </h3>
+                <h2 className='p-name'> Task Managment App </h2>
+                <div className='p-tagContainer'> 
+                <ul className='p-tag'> 
+                    <li className='p-tags'>
+                        ReactJs
+                    </li>
+                    <li className='p-tags'>
+                        Firesbase-Authentication
+                    </li>
+                    <li className='p-tags'>
+                        CSS
+                    </li>
+                    <li className='p-tags'>
+                        Github
+                    </li>
+                    <li className='p-tags'>
+                        Date-Calender Picker
+                    </li>
+                    <li className='p-tags'>
+                        Material-UI
+                    </li>
+                    <li className='p-tags'>
+                        React Router
+                    </li>
+                    <li className='p-tags'>
+                        Firebase Storage
+                    </li>
+                    <li className='p-tags'>
+                        Firebase
+                    </li>
+                    <li className='p-tags'>
+                        Firesbase-Hosting
+                    </li>
+                    <li className='p-tags'>
+                        Figma
+                    </li>
+                </ul>
+                <p className='p-description'>Working as a project manager , 
+                  I noticed I needed some sort of Task Management Platform to keep track of my daily estimates and projects .
+                  As a bonus I wanted to included some of my interest like the top 10 crypto prices and NBA scores to make the app a bit interesting.
+                  Which made me create this CRUD task management app. App includes User-Auth,Creating Tasks, Updating Task Stage, Deleting Task Stage, and 
+                  personalized User profile page.
+                  </p>
+                  <button className="p-button">
+                    <a href="#" className='p-button-ref'><span className='p-span'>View Project!</span></a>
+                  </button>
+                  </div>
+              </div>
+              <div className='p-right'>
             <a href = "https://checklistapp-2af94.web.app/" target="_blank" rel="noreferrer" className="item"><img src={CheckListApp} alt="" className="netimg"></img></a>
-            <p className='pletters'>A CRUD Web app with User Auth that Utilizes React Js and Firebase to keep track of Personalized data</p>
             </div>
-            <div className='first'>
+            </div>
+            <div className='container'>
+              <div className='p-left'>
+                <h3 className='p-text'> FULL STACK DEVELOPER </h3>
+                <h2 className='p-name'> Expense TrackR </h2>
+                <div className='p-tagContainer'> 
+                <ul className='p-tag'> 
+                    <li className='p-tags'>
+                        ReactJs
+                    </li>
+                    <li className='p-tags'>
+                        MongoDB
+                    </li>
+                    <li className='p-tags'>
+                        CSS
+                    </li>
+                    <li className='p-tags'>
+                        Github
+                    </li>
+                    <li className='p-tags'>
+                        ExpressJs
+                    </li>
+                    <li className='p-tags'>
+                        Material-UI
+                    </li>
+                    <li className='p-tags'>
+                        NodeJs
+                    </li>
+                    <li className='p-tags'>
+                        Figma
+                    </li>
+                    <li className='p-tags'>
+                        Speechly Voice Assistance
+                    </li>
+
+                </ul>
+                <p className='p-description'>Knowing how important it is to keep an accurate record of expenses but also how tedious it is typing/writing down every single expense, 
+                 I decided to embark on creating a voice assisted Expense-Tracker.
+                  </p>
+                  <button className="p-button">
+                    <a href="#" className='p-button-ref'><span className='p-span'>View Project!</span></a>
+                  </button>
+                  </div>
+              </div>
+              <div className='p-right'>
               <a href = "https://radiant-fjord-58533.herokuapp.com/" 
                 target="_blank" rel="noreferrer" className="item">
               <img src={Expensetrackr} alt="" className="netimg" /> 
               </a>
-              <p className='pletters'>A Web app built with Reactjs,MongoDB,ExpressJs that utilizes Voice assistance to keep track of spending. </p>
             </div>
-           <div className='first'>
-         <a href = "https://danielodiase.github.io/myPortfolio/" 
-               target="_blank" rel="noreferrer" className="item">
-              <img src={Portimg} alt="" className="netimg" /> 
-              </a><p className='pletters'>My Portfolio Webpage built with ReactJs,HTML,CSS and JavaScript. Get to know me a little.</p>
-           </div>
+            </div>
+            
                 
                 
-                
-         </div>
+       
          </div>
     );
   }
